576e0bbb19b7ef2811c92db4959698b8f1086e57
kyrylogrygoriev <bluejeansmonk@gmail.com>
2021-03-10 08:26:35 +0200

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+    n > 0 ? n : n = n*(-1)
+    let newArr = n.toString().split('')
+    let newStr = ''
   
+    for (let i = newArr.length; i > 0; i--){
+        newStr += newArr[i-1]
+    }
+  return Number(newStr)
 }
